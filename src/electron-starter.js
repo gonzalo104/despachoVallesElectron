3988c843a054f2e19b27d036782999e4758249db
gonzalo104 <gonzalozavalamendoza@gmail.com>
2018-08-29 19:59:03 -0500

    cross electron

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -1,15 +1,22 @@
 const electron = require('electron');
-const app = electron.app;
+const app           = electron.app;
 const BrowserWindow = electron.BrowserWindow;
-const path = require('path');
-const url = require('url');
+const path          = require('path');
+const url           = require('url');
 
 let mainWindow;
 
 function createWindow() {
+
+    const startUrl = process.env.ELECTRON_START_URL || url.format({
+        pathname: path.join(__dirname, '/../build/index.html'),
+        protocol: 'file:',
+        slashes : true
+    });
     
-    mainWindow = new BrowserWindow({width: 800, height: 600});
-    mainWindow.loadURL('http://localhost:3000');
+    mainWindow = new BrowserWindow({width: 900, height: 600});
+    //mainWindow.loadURL('http://localhost:3000');
+    mainWindow.loadURL(startUrl);
     mainWindow.webContents.openDevTools();
     mainWindow.on('closed', function () {
         mainWindow = null
