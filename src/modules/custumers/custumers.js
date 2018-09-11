275939a26f76539ecb3a26c9242cdcd0964f47ca
Gonzalo <gonzalozavalamendoza@gmail.com>
2018-09-11 00:42:15 -0500

    ui

--- src/modules/custumers/custumers.js
+++ src/modules/custumers/custumers.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Header, Table, Grid} from 'semantic-ui-react';
+import { Container, Header, Table, Grid, Dimmer, Loader, Button, Select} from 'semantic-ui-react';
 const {ipcRenderer} = window.require('electron')
 
 
@@ -20,6 +20,12 @@ const renderBodyRow = (data, i) => {
     });
 } 
 
+const options = [
+  {key: 'n', text: 'Selecciona un abogado', value: ''},
+  { key: 'm', text: 'Male', value: 'male' },
+  { key: 'f', text: 'Female', value: 'female' },
+]
+
 export default class Custumers extends Component {
   
   state = {
@@ -37,10 +43,30 @@ export default class Custumers extends Component {
   render() {
     return (
     <Container>
-    <Header as='h2'>Clientes</Header>
+      <Grid>
+      <Grid.Column width={16}>
+      <Header as='h1' floated='left'>
+        Clientes
+      </Header>
+      <Header floated='right'>
+        <Button positive>Nuevo Cliente</Button>
+      </Header>
+      <Header as='h5' floated='right'>
+      <Select placeholder='Selecciona un abogado' options={options} />
+      </Header>        
+      </Grid.Column>
+    </Grid>
+    
      <Grid>
       <Grid.Column width={16}>
-        <Table celled headerRow={headerRow} renderBodyRow={renderBodyRow} tableData={this.state.custumers} />
+        {this.state.custumers.length > 0 ? 
+          <Table celled headerRow={headerRow} renderBodyRow={renderBodyRow} tableData={this.state.custumers} />
+          :
+          <Dimmer active inverted>
+            <Loader content="Cargando..."  inline='centered' />
+          </Dimmer>
+        }
+        
       </Grid.Column>
     </Grid>
     </Container>
